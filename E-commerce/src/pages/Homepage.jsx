cd799a736ec9ce56bf16e7688a8f49e5f1ca5d14
Ankit Kandpal <Ankitkandpal210@gmail.com>
2025-09-18 13:20:34 +0530

    Fetch data from the backend

--- E-commerce/src/pages/Homepage.jsx
+++ E-commerce/src/pages/Homepage.jsx
@@ -4,6 +4,14 @@ import './Homepage.css';
 import { products } from '../../Starting-code/data/products';
 
 export function Homepage() {
+    fetch('http://localhost:3000/api/products')
+    .then((response) => {
+        response.json.then((data) => {
+          console.log(data);
+        })
+    })
+
+
   return (
     <>
       <title>HomePage</title>
